a0af809125c8b47ba822cfe810d6fc99518c6d1a
Lontsi-Hernandez <www.hernandezroma@gmail.com>
2023-04-02 12:14:53 +0200

    accueil

--- Accueil_immoprime/app/src/main/java/com/example/accueil_immo_prime/MainActivity.kt
+++ Accueil_immoprime/app/src/main/java/com/example/accueil_immo_prime/MainActivity.kt
@@ -4,25 +4,24 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,23 +53,28 @@ fun MyScreen() {
 
     Scaffold(
         topBar = {
-            ParallaxTopAppBar(
-                title = "",
+            Surface(
+                elevation = 4.dp,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .background(color = Color.Transparent)
+                        .fillMaxWidth()
+                ) {
+                    IconButton(onClick = { /* Do something */ }) {
+                        Icon(Icons.Filled.Person, "Profile")
+                    }
 
-                actions = {
-                    Row(
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        IconButton(onClick = { /* Do something */ }) {
-                            Icon(Icons.Filled.Settings, contentDescription = "Settings")
-                        }
-                        IconButton(onClick = { /* Do something */ }) {
-                            Icon(Icons.Filled.ExitToApp, contentDescription = "Logout")
-                        }
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    IconButton(onClick = { /* Do something */ }) {
+                        Icon(Icons.Filled.Favorite, "Application logo")
                     }
                 }
-            )
+            }
         },
         content = {
             Column(modifier = Modifier.verticalScroll(scrollState)) {
@@ -79,6 +83,7 @@ fun MyScreen() {
                 CardWithImageAndText()
                 CardWithImageAndText()
                 NavigationBar()
+
 
             }
         }
@@ -146,7 +151,7 @@ fun HeaderWithImage() {
             .fillMaxWidth()
     ){
         Image(
-            painter = painterResource(R.drawable.houseback),
+            painter = painterResource(R.drawable.house1),
             contentDescription = null,
             modifier = Modifier
                 .fillMaxWidth()
@@ -168,13 +173,14 @@ fun HeaderWithImage() {
         Box(
             modifier = Modifier
                 .align(Alignment.TopCenter)
-                .offset(16.dp)
+                .offset(20.dp)
+                .padding(vertical = 8.dp)
                 //.background(Color.Gray, shape = RoundedCornerShape(4.dp))
 
         ) {
             Text(
                 text = "Dicover Your Favorite House",
-
+                modifier = Modifier.padding(vertical = 8.dp),
                 style = TextStyle(fontSize = 24.sp),
             )
         }
@@ -186,48 +192,114 @@ fun HeaderWithImage() {
             verticalAlignment = Alignment.CenterVertically
         ) {
             OutlinedTextField(
+
                 value = "",
                 onValueChange = {  },
                 label = { Text("Search") },
                 modifier = Modifier.weight(1f),
-                textStyle = MaterialTheme.typography.body2.copy(color = Color.Black)
+                textStyle = MaterialTheme.typography.body2.copy(color = Color.Black),
+
             )
 
         }
     }
 }
 @Composable
+fun HousePurchaseCard() {
+    Card(
+        modifier = Modifier
+            .width(20.dp)
+            .padding(16.dp),
+        elevation = 8.dp,
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Column {
+            Image(
+                painterResource(id = R.drawable.house1),
+                contentDescription = "Appartemenent a louer situer a douala-logbessou ",
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Text(
+                text = "50.000f                         For Sale",
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier.padding(16.dp)
+            )
+
+            Text(
+                text = "Pour acheter une maison, il est important de déterminer votre budget, de trouver une propriété qui répond à vos besoins et à vos critères, de faire une offre et de négocier le contrat de vente. Vous devrez également organiser une inspection de la maison et obtenir une assurance habitation. N'oubliez pas les coûts supplémentaires tels que les frais de clôture et les taxes foncières.",
+                style = MaterialTheme.typography.body2,
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+            )
+        }
+    }
+}
+
+//Reciclerview horizontal
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
 fun HorizontalImageList() {
     val images = listOf(
-        ImageWithText(R.drawable.strawberry_pie_1, "Titre 1"),
-        ImageWithText(R.drawable.strawberry_pie_3, "Titre 2"),
-        ImageWithText(R.drawable.strawberry_pie_2, "Titre 3")
+        ImageWithText(R.drawable.house3, "Titre 1"),
+        ImageWithText(R.drawable.house4, "Titre 2"),
+        ImageWithText(R.drawable.house3, "Titre 3")
     )
 
     LazyRow(contentPadding = PaddingValues(horizontal = 16.dp)) {
         items(images) { item ->
             Card(
-                shape = RoundedCornerShape(8.dp),
-                elevation = 4.dp,
-                modifier = Modifier.padding(8.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                elevation = 8.dp,
+                onClick = {},
+                shape = RoundedCornerShape(16.dp)
             ) {
-                Column(modifier = Modifier.width(150.dp)) {
+                Column(modifier = Modifier.width(200.dp)) {
+
                     Image(
-                        painter = painterResource(id = R.drawable.strawberry_pie_2),
-                        contentDescription = null,
-                        modifier = Modifier.fillMaxWidth().height(120.dp)
+
+                        painterResource(id = R.drawable.house1),
+                        contentDescription = "Appartemenent a louer situer a douala-logbessou",
+                        modifier = Modifier
+                            .height(180.dp)
+                            .fillMaxWidth()
+                            .clip(shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)),
+                        contentScale = ContentScale.Crop
                     )
                     Text(
-                        text = "item.title",
-                        textAlign = TextAlign.Center,
+                        text = "Appartement",
+                        textAlign = TextAlign.Left,
                         fontSize = 16.sp,
-                        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp, horizontal = 10.dp)
+                    )
+                    Text(
+                        text="For Sale",
+                        color = Color.Blue,
+                        fontSize = 10.sp,
+                        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp, horizontal = 10.dp)
+
+
+                    )
+                    Text(
+                        text="50.000f",
+                        color = Color.Black,
+                        fontSize = 14.sp,
+                        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp, horizontal = 10.dp)
+
+
                     )
                     Button(
                         onClick = { /* Do something */ },
-                        modifier = Modifier.fillMaxWidth()
+
+                        modifier = Modifier.fillMaxWidth().background(Color.Transparent),
                     ) {
-                        Text(text = "Cliquez ici")
+                        Text(text = "Detail")
                     }
                 }
             }
@@ -247,7 +319,7 @@ fun CardWithImageAndText() {
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Image(
-                painter = painterResource(R.drawable.strawberry_pie_3),
+                painter = painterResource(R.drawable.houseback),
                 contentDescription = null,
                 modifier = Modifier
                     .fillMaxWidth()
@@ -255,17 +327,35 @@ fun CardWithImageAndText() {
                 contentScale = ContentScale.Crop
             )
             Text(
-                text = "Card Title",
-                style = MaterialTheme.typography.h6,
-                modifier = Modifier.padding(top = 8.dp)
+                text = "Appartement",
+                textAlign = TextAlign.Left,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
             )
-            Text(text = "Card Description")
+            Text(
+                text="For Sale",
+                color = Color.Blue,
+                fontSize = 10.sp,
+                modifier = Modifier.padding(vertical = 2.dp)
+
+
+            )
+            Text(
+                text="50.000f",
+                color = Color.Black,
+                fontSize = 14.sp,
+                modifier = Modifier.padding(vertical = 3.dp)
+
+
+            )
             Button(onClick = { /* Do something */ }, modifier = Modifier.align(Alignment.End)) {
                 Text("Read More")
             }
         }
     }
 }
+
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
