16bb4a577d1029a4ea0b148f3af593ba49517a26
Nilisofficiel <versselsiyapze@gmail.com>
2023-05-12 03:27:23 +0100

    gestion d'erreur pour  map!

--- app/src/main/java/com/example/immo_prime/MapsActivity.kt
+++ app/src/main/java/com/example/immo_prime/MapsActivity.kt
@@ -10,72 +10,90 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.common.api.GoogleApi
 import com.google.android.gms.maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var mMap: GoogleMap
+
+    companion object{
+        private  const val LOCATION_PERMISSION_REQUEST_CODE =123
+    }
+
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        val mapView = findViewById<MapView>(R.id.map_view)
-        mapView.getMapAsync { googleMap ->
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                googleMap.isMyLocationEnabled = true
-                fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    fun setupMap() {
+        val mapView = findViewById<MapView>(R.id.mapView)
+        mapView.getMapAsync { mMap ->
+            if (ActivityCompat.checkSelfPermission(
+                    this, Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                mMap.isMyLocationEnabled = true
+                fusedLocationClient.lastLocation.addOnSuccessListener { location ->
                     location?.let {
-                        val currentLatLng = LatLng(location.latitude, location.longitude)
-                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+                        val currentLatLng = LatLng(it.latitude, it.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
                     }
                 }
             }
         }
-}
+
+    }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mMap.isMyLocationEnabled = true
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                location?.let {
-                    val currentLatLng = LatLng(location.latitude, location.longitude)
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
-                }
-            }
-        } else {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
-        }
+        setupMap()
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    mMap.isMyLocationEnabled = true
-                    fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                        location?.let {
-                            val currentLatLng = LatLng(location.latitude, location.longitude)
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
-                        }
-                    }
-                }
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted
+                setupMap()
             } else {
-                // Afficher un message ou une boîte de dialogue pour informer l'utilisateur que la permission a été refusée
+                showPermissionDeniedAlert {
+                    finish()
+                }
             }
         }
     }
 
-    companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 123
+    private fun showPermissionDeniedAlert(onDismiss: () -> Unit) {
+        AlertDialog.Builder(this)
+            .setMessage("Location permission is needed to access this feature. Please allow location permission in app settings.")
+            .setPositiveButton("App settings") { _, _ ->
+                startAppSettings()
+            }
+            .setNegativeButton("Exit") { _, _ ->
+                onDismiss()
+            }
+            .show()
+    }
+
+    private fun startAppSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivity(intent)
     }
 }
+
--- app/src/main/java/com/example/immo_prime/homeScreen.kt
+++ app/src/main/java/com/example/immo_prime/homeScreen.kt
@@ -1,235 +1,67 @@
 package com.example.immo_prime
 
 import android.Manifest
-import android.app.Activity
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Location
-import android.net.Uri
-import android.provider.Settings
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.model.LatLng
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-import com.google.android.gms.maps.MapsInitializer
 
+
+// ...
 
 @Composable
 fun MysScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box(
-            modifier = Modifier.weight(1f)
-        ) {
-            // Définir l'en-tête ici
-
-        }
-
-        // Box 3: Description with price
-        Box(modifier = Modifier.weight(1f)) {
-            // Définir la description et le prix ici
-        }
-
-        // Box 2: TabBar with map and localisation
-        Box(modifier = Modifier.weight(1f)) {
-            val tabs = listOf("MAP", "INFORMATIONS")
-            var selectedTabIndex by remember { mutableStateOf(0) }
-            TabRow(selectedTabIndex) {
-                tabs.forEachIndexed { index, title ->
-                    Tab(
-                        text = { Text(title) },
-                        selected = selectedTabIndex == index,
-                        onClick = { selectedTabIndex = index }
-                    )
-                }
-            }
-            when (selectedTabIndex) {
-                0 -> {
-                    val context = LocalContext.current
-                    val mapView = rememberMapViewWithLifecycle()
-                    var isPermissionGranted by remember { mutableStateOf(false) }
-                    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
-
-                    if (!isPermissionGranted) {
-                        /*Button(
-                            onClick = {*/
-                                requestPermission(
-                                    Manifest.permission.ACCESS_FINE_LOCATION,
-                                    LOCATION_PERMISSION_REQUEST_CODE
-                                )
-                            /*},
-                            modifier = Modifier
-                                .align(Alignment.BottomEnd)
-                                .padding(16.dp)
-                        ) {
-                            Text(text = "Autoriser la géolocalisation")
-                        }*/
-                    }
-
-                    AndroidView({ mapView }) { mapView ->
-                        mapView.getMapAsync { googleMap ->
-                            MapsInitializer.initialize(context)
-                            googleMap.uiSettings.isMyLocationButtonEnabled = false
-                            googleMap.uiSettings.isZoomControlsEnabled = true
-
-                            if (ActivityCompat.checkSelfPermission(
-                                    context,
-                                    Manifest.permission.ACCESS_FINE_LOCATION
-                                ) == PackageManager.PERMISSION_GRANTED
-                            ) {
-                                googleMap.isMyLocationEnabled = true
-                                fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                                    location?.let {
-                                        val currentLatLng = LatLng(location.latitude, location.longitude)
-                                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
-                                    }
-                                }
-                            } else {
-                                // Demander la permission de localisation
-                                ActivityCompat.requestPermissions(
-                                    context as Activity,
-                                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                                    LOCATION_PERMISSION_REQUEST_CODE
-                                )
-                            }
-                        }
-                    }
-                }
-                1 -> {
-                    // Ajouter les informations ici
-                    Text(
-                        text = "Cette Maison  est situé à Douala plus précisement à Logbessou derrière iuc à 100m de la route",
-                        modifier = Modifier.padding(50.dp)
-                    )
-                }
+    // ...
+    // Box 2: TabBar with map and localisation
+    Box(modifier = Modifier.fillMaxSize()) {
+        val tabs = listOf("MAP", "INFORMATIONS")
+        var selectedTabIndex by remember { mutableStateOf(0) }
+        TabRow(selectedTabIndex) {
+            tabs.forEachIndexed { index, title ->
+                Tab(
+                    text = { Text(title) },
+                    selected = selectedTabIndex == index,
+                    onClick = { selectedTabIndex = index }
+                )
             }
         }
-
-        //@Composable
-        /*private fun rememberMapViewWithLifecycle(): MapView {
-            val context = LocalContext.current
-            val mapView = remember {
-                MapView(context).apply {
-                    id = R.id.map_view
-                }
+        when (selectedTabIndex) {
+            0 -> {
+                // Check for location permission and request if needed
+                checkLocationPermissionAndShowMap()
             }
-
-            val lifecycleOwner = LocalLifecycleOwner.current
-            DisposableEffect(lifecycleOwner) {
-                val observer = object : DefaultLifecycleObserver {
-                    override fun onResume(owner: LifecycleOwner) {
-                        mapView.onResume()
-                    }
-
-                    override fun onPause(owner: LifecycleOwner) {
-                        mapView.onPause()
-                    }
-
-                    override fun onDestroy(owner: LifecycleOwner) {
-                        mapView.onDestroy()
-                    }
-                }
-                lifecycleOwner.lifecycle.addObserver(observer)
-
-                onDispose {
-                    lifecycleOwner.lifecycle.removeObserver(observer)
-                }
+            1 -> {
+                // Show information
             }
-
-            return mapView
-        }*/
-
-        // Box 4: Description with price
-        Box(modifier = Modifier.weight(1f)) {
-            // Définir la description ici
-        }
-
-        // Ajouter les boutons de navigation ici
-        BottomNavigation {
-            BottomNavigationItem(
-                icon = { Icon(Icons.Filled.Home, contentDescription = null) },
-                selected = true,
-                onClick = {}
-            )
-            BottomNavigationItem(
-                icon = { Icon(Icons.Filled.Search, contentDescription = null) },
-                selected = false,
-                onClick = {}
-            )
-            BottomNavigationItem(
-                icon = { Icon(Icons.Filled.ShoppingCart, contentDescription = null) },
-                selected = false,
-                onClick = {}
-            )
-            BottomNavigationItem(
-                icon = { Icon(Icons.Filled.Settings, contentDescription = null) },
-                selected = false,
-                onClick = {}
-            )
-            BottomNavigationItem(
-                icon = { Icon(Icons.Filled.LocationOn, contentDescription = null) },
-                selected = false,
-                onClick = {}
-            )
         }
     }
+
+    // ...
 }
+
 @Composable
-private fun rememberMapViewWithLifecycle() : MapView {
+fun checkLocationPermissionAndShowMap() {
     val context = LocalContext.current
-    val mapView = remember {
-        MapView(context).apply {
-            id = R.id.map_view
+    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        // Request permission
+        requestPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    } else {
+        // Show map
+        AndroidView(factory = { context -> MapView(context).apply { id = R.id.mapView } }) { mapView ->
+            // Implement map initialization and location updates here
         }
     }
-
-    val lifecycleOwner = LocalLifecycleOwner.current
-    DisposableEffect(lifecycleOwner) {
-        val observer = object : DefaultLifecycleObserver {
-            override fun onResume(owner: LifecycleOwner) {
-                mapView.onResume()
-            }
-
-            override fun onPause(owner: LifecycleOwner) {
-                mapView.onPause()
-            }
-
-            override fun onDestroy(owner: LifecycleOwner) {
-                mapView.onDestroy()
-            }
-        }
-        lifecycleOwner.lifecycle.addObserver(observer)
-
-        onDispose {
-            lifecycleOwner.lifecycle.removeObserver(observer)
-        }
-    }
-
-    return mapView
 }
-
 
 @Composable
-private fun requestPermission(permission: String, requestCode: Int) {
-    LocalContext.current.startActivity(
-        Intent(
-            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-            Uri.parse("package:${LocalContext.current.packageName}")
-        )
-    )
+fun requestPermission(permission: String) {
+    // Start settings activity to let user enable permission
 }
-
-private const val LOCATION_PERMISSION_REQUEST_CODE = 123
