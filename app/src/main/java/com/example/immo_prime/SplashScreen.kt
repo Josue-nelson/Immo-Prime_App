149c432b8703bfae33cf97ce7fe72c99796b1cd4
Evrard237-sudo <114961933+Evrard237-sudo@users.noreply.github.com>
2023-05-13 10:09:05 +0100

    Ajout de L'interface ajoutez logement

--- app/src/main/java/com/example/immo_prime/SplashScreen.kt
+++ app/src/main/java/com/example/immo_prime/SplashScreen.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.immo_prime.Animations.LoadingAnimation
+import com.example.immo_prime.Animations_object.LoadingAnimation
 import kotlinx.coroutines.delay
 
 @Composable
