f57b366fbc2022ead1fd48660d670d50a62c16ac
Nilisofficiel <versselsiyapze@gmail.com>
2023-05-19 20:21:10 +0100

    intrface payement

---
+++ app/src/main/java/com/example/immo_prime/Payement/AddPaymentCard.kt
@@ -0,0 +1,173 @@
+package com.example.immo_prime.Payement
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import com.example.immo_prime.R
+import com.jetpack.paymentcarddetail.Payement.PaymentCard
+
+@ExperimentalAnimationApi
+@Composable
+fun AddPaymentCard() {
+    var nameText by remember { mutableStateOf(TextFieldValue()) }
+    var cardNumber by remember { mutableStateOf(TextFieldValue()) }
+    var expiryNumber by remember { mutableStateOf(TextFieldValue()) }
+    var cvcNumber by remember { mutableStateOf(TextFieldValue()) }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        PaymentCard(
+            nameText,
+            cardNumber,
+            expiryNumber,
+            cvcNumber
+        )
+        LazyColumn(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            item {
+                InputItem(
+                    textFieldValue = nameText,
+                    label = stringResource(id = R.string.card_holder_name),
+                    onTextChanged = { nameText = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp)
+                )
+            }
+
+            item {
+                InputItem(
+                    textFieldValue = cardNumber,
+                    label = stringResource(id = R.string.card_holder_number),
+                    keyboardType = KeyboardType.Number,
+                    onTextChanged = { cardNumber = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp),
+                    visualTransformation = CreditCardFilter
+                )
+            }
+
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    InputItem(
+                        textFieldValue = expiryNumber,
+                        label = stringResource(id = R.string.expiry_date),
+                        keyboardType = KeyboardType.Number,
+                        onTextChanged = { expiryNumber = it },
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(end = 8.dp)
+                    )
+                    InputItem(
+                        textFieldValue = cvcNumber,
+                        label = stringResource(id = R.string.cvc),
+                        keyboardType = KeyboardType.Number,
+                        onTextChanged = { cvcNumber = it },
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(start = 8.dp)
+                    )
+                }
+            }
+
+            item {
+                Button(
+                    onClick = { },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.save),
+                        color = MaterialTheme.colors.onPrimary,
+                        modifier = Modifier.padding(horizontal = 30.dp, vertical = 8.dp)
+                    )
+                }
+            }
+
+
+            item {
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 8.dp)
+                ){
+                    Text(
+                        text = "Autres moyens de payement ",
+                        style = MaterialTheme.typography.h6,
+                        color = Color.Blue,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+
+            item {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp)
+                        .background(Color.White)
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .width(100.dp)
+                            .background(Color.White)
+                    ) {
+                        Button(
+                            onClick = { /* action on button 1 click */ },
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .height(48.dp)
+                                .background(Color.White)
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.momo),
+                                contentDescription = "Button 1 Image",
+                                modifier = Modifier.fillMaxSize()
+                            )
+                        }
+                        Button(
+                            onClick = { /* action on button 2 click */ },
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .height(48.dp)
+                                .background(Color.White)
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.orage_money),
+                                contentDescription = "Button 2 Image",
+                                modifier = Modifier.fillMaxSize()
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/immo_prime/Payement/OMview.kt
@@ -0,0 +1,70 @@
+package com.jetpack.paymentcarddetail.Payement
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MyOmBox() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.orage_money),
+                contentDescription = "Orange Money Logo",
+                modifier = Modifier.size(64.dp)
+            )
+            Text(
+                text = "Entrez votre numéro de téléphone",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Numéro de téléphone") },
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+            )
+            Button(
+                onClick = { /* action on button click */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(text = "Valider")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OMScreenPreview(){
+    MyOmBox()
+
+
+}
